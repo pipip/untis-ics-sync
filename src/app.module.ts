37ad775f739d746d105dcd49831abb260a97a9aa
pipip <philipp.schroeder@gmail.com>
2026-09-23 21:28:45 +0200

    Fix lint #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { NotificationsModule } from './notifications/notifications.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    ScheduleModule.forRoot(), 
+    ScheduleModule.forRoot(),
     BullModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
